28d3196e5faf59cf7753f3c8ee6b0ccfde217aed
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-02-04 11:19:59 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,8 +8,12 @@
 		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
 		<link rel="stylesheet" href="css/styles.css" />
 		<!-- STEP 1: Save this file as index.php instead of index.html -->
-		<!-- STEP 2: Connect to your Web server, and upload all of the files in this lab to a new folder called 'lab4' inside the 'week4' directory we created during this week's lesson -->
-		<!-- STEP 3: Make sure that you can browse to this page on the server - the URL that you would have to enter into the address bar in a browser might look something like https://mysitename.azurewebsites.net/week4/lab4/index.php (for Microsoft Azure) -->
+		<!-- STEP 2: Connect to your Web server, and upload all of the files in this lab to a new folder called 
+		'lab4' inside the 'week4' directory we created during this week's lesson -->
+		<!-- STEP 3: Make sure that you can browse to this page on the server - 
+		the URL that you would have to enter into the address bar in a browser
+		 might look something like https://mysitename.azurewebsites.net/week4/lab4/index.php (for Microsoft Azure) -->
+		 <!-- Created the Lab04SGouveia under XAMPP/HTDOCS/WEEK4-->
 	</head>
 	<body>
 		<!-- Page-level header -->
@@ -41,23 +45,31 @@
 			</section>
 			<!-- Side Notes / Related Information -->
 			<aside>
+			
 <!-- STEP 4: Create a pair of FORM tags so that visitors can sign up for a newsletter -->
-
-<!-- STEP 5: Add an action attribute to the FORM start tag that points to a file called "subscribe.php", and set the method attribute to "get" -->
+<!-- STEP 5: Add an action attribute to the FORM start tag that points to a file called "subscribe.php", 
+and set the method attribute to "get" -->
+			<form action="subscribe.php" method=GET>     	<!--Opening the FORM -->
 <!-- STEP 6: Add an H3 element inside the FORM tags that reads, "Subscribe to Newsletter" -->
-
+				<h3>Subscribe to Newsletter</h3> 			<!-- Created the heading3 -->
 <!-- STEP 7a: Add a LABEL element for the email address -->
-
-<!-- STEP 7b: Add an INPUT element for the user to type their email address - set the type of INPUT to "email", add a name attribute that is set to "emailAddress", and an id attribute that is set to "emailAddress" -->
-
-<!-- STEP 7c: Add the 'for' attribute to the above LABEL element, and set the value to match the 'id' value of the INPUT element -->
-<!-- STEP 7d: Include the 'required' attribute on the above INPUT element so that users are forced to enter an email address before the form is submitted -->
+				<label for="emailAddress">Email:</label>	<!--added Label, the for attribute -->
+<!-- STEP 7b: Add an INPUT element for the user to type their email address - set the type of INPUT to "email", 
+add a name attribute that is set to "emailAddress", and an id attribute that is set to "emailAddress" -->
+				<input type="email" id="emailAddress" name="emailAddress" required/> 	<!--Added the input, required (7d)-->
+<!-- STEP 7c: Add the 'for' attribute to the above LABEL element, and set the value to match the 'id' 
+value of the INPUT element -->
+<!-- STEP 7d: Include the 'required' attribute on the above INPUT element so that users are forced to enter an 
+email address before the form is submitted -->
 <!-- STEP 8a: Add a BUTTON element with the attribute type="submit" -->
-					
+				<button type="submit"> Subscribe </button>		<!--Added the button with Subscribe -->
 <!-- STEP 8b: Set the content of the BUTTON element to read, "Subscribe" -->
-<!-- STEP 8c: Upload the changed index.php file that you've now edited to the server and refresh the page to view the changes - try adding an email address and submitting the form -->
+<!-- STEP 8c: Upload the changed index.php file that you've now edited to the server and refresh the page 
+to view the changes - try adding an email address and submitting the form -->
+				<!-- Sent the email and the page is redirecting to http://localhost/week4/Lab4/subscribe.php?emailAddress=sergiocutrim%40gmail.com-->
+				<!-- The opened page has Thank you for Subscribing -->
 <!-- STEP 9: Open up subscribe.php in your code editor and head to STEP 10 -->
-
+			</form>
 				<h3>Did You Know…?</h3>
 				<p>One of the most important components of effective life-long learning is <em>play</em>. Learning a new skill (like Web development) should include a certain amount of play. Renowned children’s programming entertainer Mr. Rogers certainly knew about the value that play represented:</p>
 				<!-- Quotation -->
